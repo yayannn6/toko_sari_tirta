b96b6d0c1016b5dd0e7a2ceebea8331e03b0a2ee
Yayan H <yayannn.hd@gmail.com>
2025-12-31 18:57:27 +0700

    add diskon

--- custom_invoice_receipt/models/sales_order_line.py
+++ custom_invoice_receipt/models/sales_order_line.py
@@ -10,16 +10,41 @@ class SaleOrderLine(models.Model):
         store=True,
     )
 
-    @api.onchange('discount_amount', 'product_uom_qty', 'price_unit')
-    def _onchange_discount_amount(self):
-        for line in self:
-            discount = line.discount_amount or 0.0
-            after_discount_price = line.price_unit * line.product_uom_qty - discount
-            line.price_subtotal = after_discount_price
-
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
+
+    total_discount_amount = fields.Monetary(
+        string='Total Discount Amount',
+        currency_field='currency_id',
+        digits='Product Price',
+        compute='_compute_total_discount_amount',
+        store=True,
+    )
+
+    @api.depends('order_line.discount_amount')
+    def _compute_total_discount_amount(self):
+        for order in self:
+            order.total_discount_amount = sum(
+                order.order_line.mapped('discount_amount')
+            )
+
+    @api.depends(
+        'order_line.price_subtotal',
+        'order_line.discount_amount',
+        'currency_id'
+    )
+    def _compute_amounts(self):
+        super()._compute_amounts()
+
+        for order in self:
+            discount = sum(order.order_line.mapped('discount_amount'))
+
+            # kurangi untaxed dengan discount
+            order.amount_untaxed -= discount
+
+            # total = untaxed + tax
+            order.amount_total = order.amount_untaxed + order.amount_tax
 
     def action_print_receipt(self):
         """
